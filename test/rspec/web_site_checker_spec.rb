422946e5df71c62e4a7b2f90365b5a721156c220
Lauri Lehmijoki <lauri.lehmijoki@iki.fi>
2012-02-23 21:50:26 +0200

    Be independent of the logger implementation

--- test/rspec/web_site_checker_spec.rb
+++ test/rspec/web_site_checker_spec.rb
@@ -1,19 +1,9 @@
 require 'rspec'
 require File.dirname(__FILE__)+"/../../lib/web_site_checker"
 
-class Logger
-  def log_success(hash)
-
-  end
-
-  def log_failure(hash)
-    puts hash
-  end
-end
-
 describe WebSiteChecker do
   it "logs a non-exiting host as failure" do
-    logger = Logger.new
+    logger = mock(:logger)
     logger.should_receive(:log_failure).once
     
     checker = WebSiteChecker.new(["http://foo"], logger) 
@@ -21,7 +11,7 @@ describe WebSiteChecker do
   end
 
   it "it logs Google front page as success" do
-    logger = Logger.new
+    logger = mock(:logger)
     logger.should_receive(:log_success).once
     
     checker = WebSiteChecker.new(["http://google.fi"], logger) 
