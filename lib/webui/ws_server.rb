1596e46604c9072e5c0d269d45fb5c5a806f77d3
Lauri Lehmijoki <lauri.lehmijoki@iki.fi>
2012-02-28 18:52:43 +0200

    CLI command for running the WebSocket server

--- lib/webui/ws_server.rb
+++ lib/webui/ws_server.rb
@@ -1,11 +1,17 @@
 require 'rubygems'
 require 'em-websocket'
 
-$sockets = []
-Thread.new do
-  EventMachine.run {
+class WSServer
+  @@port = 8888
+  def self.port
+    @@port
+  end
+  def self.run
+    $sockets = []
+    Thread.new do
+      EventMachine.run {
 
-    EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 8888) do |socket|
+        EventMachine::WebSocket.start(:host => "0.0.0.0", :port => @@port) do |socket|
         socket.onopen {
           $sockets << socket
         }
@@ -13,35 +19,37 @@ Thread.new do
         socket.onclose {
           $sockets.delete socket
         }
-    end
-  }
-end
-
-def send_logs_to_sockets(logs)
-  puts "Discovered #{logs.length} new log entries from the database. Sending to #{$sockets.length} clients..." unless logs.empty?
-  $sockets.each do |socket| 
-    socket.send(logs.to_json) unless logs.empty?
-  end
-end
-
-Thread.new do
-  require 'json'
-  require File.dirname(__FILE__)+"/../common/database"
-  begin
-    interval = 5
-    last_run = Time.new + interval
-    while true
-      sleep(interval)
-      Region.all_regions.each{ |region|
-        logs = Database.new.load_url_logs(last_run, region)
-        send_logs_to_sockets(logs)
+        end
       }
-      last_run = Time.new
     end
-  rescue Exception => e
-    puts "Encountered a problem: #{e.message}"
+
+    def self.send_logs_to_sockets(logs)
+      puts "Discovered #{logs.length} new log entries from the database. Sending to #{$sockets.length} clients..." unless logs.empty?
+      $sockets.each do |socket| 
+        socket.send(logs.to_json) unless logs.empty?
+      end
+    end
+
+    Thread.new do
+      require 'json'
+      require File.dirname(__FILE__)+"/../common/database"
+      begin
+        interval = 5
+        last_run = Time.new + interval
+        while true
+          sleep(interval)
+          Region.all_regions.each{ |region|
+            logs = Database.new.load_url_logs(last_run, region)
+            WSServer.send_logs_to_sockets(logs)
+          }
+          last_run = Time.new
+        end
+      rescue Exception => e
+        puts "Encountered a problem: #{e.message}"
+      end
+
+    end
+
+    sleep
   end
-
 end
-
-sleep
